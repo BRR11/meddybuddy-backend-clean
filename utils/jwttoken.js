1cea5b010443aaad1b95a1aeb3eb54dc0844b1d2
BRR11 <90711231+BRR11@users.noreply.github.com>
2025-07-14 00:55:11 +0530

    Update jwttoken.js

--- utils/jwttoken.js
+++ utils/jwttoken.js
@@ -3,7 +3,9 @@ const sendToken = (user,statusCode,res)=>{
 
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE*24*60*60*60*1000),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: "None",    // Important for cross-origin
+        secure: true 
     };
 
 
