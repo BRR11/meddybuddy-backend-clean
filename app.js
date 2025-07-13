6854c708b6ed925756dc67e28f270a39b461b0f4
BRR11 <90711231+BRR11@users.noreply.github.com>
2025-07-13 22:03:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
 const express = require("express");
 const app = express();
 
-const errorMiddleWare = require("../backend/middleware/error.js");
+const errorMiddleWare = require("./middleware/error.js");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
     origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 }));
-const {config} = require("dotenv");
-config({path: "./config/config.env"});
-//Route Import
-const products = require("../backend/routes/productsRoute.js");
+
+const { config } = require("dotenv");
+config({ path: "./config/config.env" });
+
+// Route Imports
+const products = require("./routes/productsRoute.js");
 const Errorhandler = require("./utils/errorhandler.js");
-const user = require("../backend/routes/userRoutes.js");
-const payment = require("../backend/routes/paymentRoutes.js");
-const order = require('../backend/routes/orderRoutes.js')
-const cart = require("../backend/routes/cartRoutes.js")
+const user = require("./routes/userRoutes.js");
+const payment = require("./routes/paymentRoutes.js");
+const order = require("./routes/orderRoutes.js");
+const cart = require("./routes/cartRoutes.js");
+
 app.use(cookieParser());
 
-
-app.use("/api/v1",products);
-app.use("/api/v1",user);
-app.use("/api/v1",order);
-app.use("/api/v1",payment);
-app.use("/api/v1",cart)
-
+app.use("/api/v1", products);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+app.use("/api/v1", cart);
 
 app.use(errorMiddleWare);
 
-
 module.exports = app;
-
-
-
