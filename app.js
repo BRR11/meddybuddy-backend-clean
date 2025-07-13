f0a601190557cc471d37ff8d3b73855c81175f0b
BRR11 <90711231+BRR11@users.noreply.github.com>
2025-07-14 00:46:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
     origin: [
-        "http://localhost:3000",
         "https://meddybuddy-ecommerce-3170maxzd-rishanth-reddys-projects.vercel.app"
     ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    credentials: true
 }));
+
 
 
 const { config } = require("dotenv");
